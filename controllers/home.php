2c380921e941d607b0ef6259ae9f583709f96054
nathangrigne <nathan.grigne@hetic.net>
2019-12-11 22:57:11 +0100

    update controller home

--- controllers/home.php
+++ controllers/home.php
@@ -1,5 +1,16 @@
 <?php
 $title = 'Home';
 
+$medocs = $pdo->query('SELECT * FROM cis_cip_bdpm LIMIT 10');
+$medocs = $medocs->fetchAll();
+
+$name = [];
+
+foreach($medocs as $_medocs){
+    $code_cis = $_medocs->code_cis;
+    $temp = $pdo->query('SELECT denomination_medicament FROM cis_bdpm WHERE code_cis='.$code_cis);
+    $temp = $temp->fetch();
+    array_push($name, $temp);
+};
 
 include '../views/pages/home.php';
