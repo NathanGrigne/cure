4470501a2bcec26d3cdf1b6f6a3ab054e6c5855e
nathangrigne <nathan.grigne@hetic.net>
2019-12-12 17:41:45 +0100

    add view team page

---
+++ views/pages/team.php
@@ -0,0 +1,58 @@
+<?php include '../views/partials/header.php' ?>
+<link rel="stylesheet" href="<? URL ?>assets/styles/team.css">
+
+    <main class="team">
+      <h1 class="team-title">L'Équipe</h1>
+      <section class="identity-container">
+        <div class="image-team">
+          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate1">-->
+        </div>
+        <div class="identity">
+          <h2>Antoine</h2>
+          <h2>Baudot</h2>
+          <h4>Designer</h4>
+        </div>
+      </section>
+      <section class="identity-container">
+        <div class="image-team">
+          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate2">-->
+        </div>
+        <div class="identity">
+          <h2>louis</h2>
+          <h2>gazel</h2>
+          <h4>Designer</h4>
+        </div>
+      </section>
+      <section class="identity-container">
+        <div class="image-team">
+          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate3">-->
+        </div>
+        <div class="identity">
+          <h2>nathan</h2>
+          <h2>grigné</h2>
+          <h4>Designer</h4>
+        </div>
+      </section>
+      <section class="identity-container">
+        <div class="image-team">
+          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate4">-->
+        </div>
+        <div class="identity">
+          <h2>anthony</h2>
+          <h2>vila</h2>
+          <h4>Designer</h4>
+        </div>
+      </section>
+      <section class="identity-container">
+        <div class="image-team">
+          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate5">-->
+        </div>
+        <div class="identity">
+          <h2>aurelien</h2>
+          <h2>sarrat</h2>
+          <h4>Designer</h4>
+        </div>
+      </section>
+    </main>
+    
+<?php include '../views/partials/footer.php' ?>
