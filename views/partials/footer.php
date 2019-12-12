a70c0a21b0a38fa219791e26a34c8e973c76826a
nathangrigne <nathan.grigne@hetic.net>
2019-12-12 22:54:17 +0100

    update view footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,9 @@
     <footer>
         
     </footer>
+    <? if($q != 'welcome'): ?>
     <script src="<?= URL ?>assets/scripts/script.js"></script>
+    <? endif; ?>
+    <script src="<? URL ?>assets/scripts/<?= $q ?>.js"></script>
 </body>
 </html>
