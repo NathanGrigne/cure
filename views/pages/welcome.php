684024c6c6c4ffd64fb290662c1ee65538d352f7
nathangrigne <nathan.grigne@hetic.net>
2019-12-12 22:53:47 +0100

    update view welcome

--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" type="image/png" href="<?= URL ?>/assets/images/favicon.png"/>
-        <title><?= !empty($title) ? $title : 'Title is empty' ?></title>
-        <link rel="stylesheet" href="<?= URL ?>assets/styles/welcome.css">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900&display=swap" rel="stylesheet">
-    </head>
-    <body>
+<?php include './views/partials/header.php' ?>
         <main class="main">
             <div class="first-page">
                 <div class="logo-container">
@@ -22,7 +11,7 @@
                 <div class="down-arrow"></div>
             </div>
             <div class="second-page">
-            <div class="logo-container">
+                <div class="logo-container">
                     <img src="<?= URL ?>assets/images/medocs_homepage.png" alt="" class="logo-welcome">
                 </div>
                 <div class="text-container">
@@ -32,6 +21,4 @@
                 <button class="let-go"><a class="link-let-go" href="<?= URL ?>home">C'est parti !</a></button>
             </div>
         </main>
-        <script src="<?= URL ?>assets/scipts/welcome.js"></script>
-    </body>
-    </html>
+<?php include './views.partials.footer?php' ?>
