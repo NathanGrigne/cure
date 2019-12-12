3f0ea02b0a8899c18fc6b1629ace7b5dd0b7ff56
nathangrigne <nathan.grigne@hetic.net>
2019-12-12 18:20:55 +0100

    update view home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,12 +1,61 @@
 <?php include '../views/partials/header.php' ?>
+<link rel="stylesheet" href="<? URL ?>assets/styles/home.css">
 
-<main>
-    <?php foreach($medocs as $index=>$_medocs): ?>
-    <div class="aled">
-        <h3><?= $name[$index]->denomination_medicament ?></h3>
-        <p><?= $_medocs->code_cip13 ?></p>
-    </div>
-    <?php endforeach; ?>
-</main>
+    <main>
+        <div class="containerIllustrationSearch">
+            <div class="illustrationSearch">
+            </div>
+            <div class="containerScrollProject">
+                <h1>Notre Project</h1>
+                <div class="scrollImage">
+                </div>
+            </div>
+        </div>
+        <div class="containerProject">
+            <article class="containerArticleProject">
+                <div class="containerTextProject">
+                    <h3 class="titleArticleProject">Qui somme nous ?</h3>
+                    <p class="textArticle">
+                        Cure est une application web 100% indépendante et gratuite.
+                    </p>
+                    <p class="textArticle">
+                        Elle n’est par conséquent pas influencé et influençable ni par les laboratoires pharmaceutiques,
+                        ni par les médecins.
+                    </p>
+                    <p class="textArticle">
+                        Elle a pour but de renseigner la population sur les médicaments qu'elle utilise.
+                        Soyons acteur de nos soins.
+                    </p>
+                </div>
+            </article>
+            <article class="containerArticleProject">
+                <div class="containerTextProject">
+                    <h3 class="titleArticleProject">Pourquoi ?</h3>
+                    <p class="textArticle">
+                        80% de la population française a recours à l’automédication, mais sans
+                        forcément connaitre les réels risques de certains médicaments.
+                    </p>
+                    <p class="textArticle">
+                        Les notices d’utilisation utilisent un vocabulaire complexe et non adapté au consommateur moyen.
+                    </p>
+                    <p class="textArticle">
+                        Cure veut donc permettre à chacun de comprendre facilement les notices d’utilisation et avoir
+                        accès aux informations primordiales au soin facilement.
+                    </p>
+                </div>
+            </article>
+            <article class="containerArticleProject">
+                <div class="containerTextProject">
+                    <h3 class="titleArticleProject">Comment ?</h3>
+                    <p class="textArticle">
+                            Notre équipe lit des notices et se renseigne chaque jour sur plusieurs médicaments afin de vous fournir les meilleures et les plus récentes informations sur chaque fiche.
+                    </p>
+                    <p class="textArticle">
+                        Chaque fiche est composée de plusieurs informations comme le taux de remboursement, le besoin ou non d’ordonnance, les symptômes, le mode d'emploi, la composition ou encore les effets secondaires.             
+                    </p>
+                </div>
+            </article>
+        </div>
+    </main>
 
 <?php include '../views/partials/footer.php' ?>
