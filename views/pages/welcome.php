30be194939702edace2d8f82ab4afa99206cc6cb
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2019-12-13 13:47:01 +0100

    modif div logo welcome

--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -2,11 +2,11 @@
         <main class="main">
             <div class="first-page">
                 <div class="logo-container">
-                    <img src="<?= URL ?>assets/images/logo_welcome.png" alt="" class="logo-welcome">
+                    <!--<img src="<?= URL ?>assets/images/logo_welcome.png" alt="" class="logo-welcome">-->
                 </div>
                 <div class="text-container">
                     <h1 class="title-welcome">Bienvenue !</h1>
-                    <p class="text-welcome">Découvre en plus sur tes médicaments!</p>
+                    <p class="text-welcome">Viens découvrir si tu utilise correctement tes médicaments </p>
                     <button class="let-go"><a class="link-let-go" href="<?= URL ?>home">C'est parti !</a></button>
                 </div>
             </div>
