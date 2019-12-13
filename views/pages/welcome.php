973168f9ef2a2089b2034eda3ea8a4e4b05bddff
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2019-12-13 10:33:39 +0100

    modification content welcome page

--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -6,19 +6,9 @@
                 </div>
                 <div class="text-container">
                     <h1 class="title-welcome">Bienvenue !</h1>
-                    <p class="text-welcome">Cure est une appli  100% indépendante qui vous facilite l’accès à l’information pharmaceutiques</p>
+                    <p class="text-welcome">Découvre en plus sur tes médicaments!</p>
+                    <button class="let-go"><a class="link-let-go" href="<?= URL ?>home">C'est parti !</a></button>
                 </div>
-                <div class="down-arrow"></div>
-            </div>
-            <div class="second-page">
-                <div class="logo-container">
-                    <img src="<?= URL ?>assets/images/medocs_homepage.png" alt="" class="logo-welcome">
-                </div>
-                <div class="text-container">
-                    <h1 class="title-welcome-2">Découvre en plus sur tes médicaments</h1>
-                    <p class="text-welcome">Cure propose une fiche simple pour comprendre facilement votre prise de soins</p>
-                </div>
-                <button class="let-go"><a class="link-let-go" href="<?= URL ?>home">C'est parti !</a></button>
             </div>
         </main>
 <?php include './views.partials.footer?php' ?>
