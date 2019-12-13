953dd5d17627795d95b2d6494aad2ae3387324a7
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2019-12-13 20:43:00 +0100

    add 'qui somme nous' team page

--- views/pages/team.php
+++ views/pages/team.php
@@ -1,57 +1,77 @@
 <?php include './views/partials/header.php' ?>
 
-    <main class="team">
-      <h1 class="team-title">L'Équipe</h1>
-      <section class="identity-container">
-        <div class="image-team">
-          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate1">-->
-        </div>
-        <div class="identity">
-          <h2>Antoine</h2>
-          <h2>Baudot</h2>
-          <h4>Designer</h4>
-        </div>
-      </section>
-      <section class="identity-container">
-        <div class="image-team">
-          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate2">-->
-        </div>
-        <div class="identity">
-          <h2>louis</h2>
-          <h2>gazel</h2>
-          <h4>Designer</h4>
-        </div>
-      </section>
-      <section class="identity-container">
-        <div class="image-team">
-          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate3">-->
-        </div>
-        <div class="identity">
-          <h2>nathan</h2>
-          <h2>grigné</h2>
-          <h4>Designer</h4>
-        </div>
-      </section>
-      <section class="identity-container">
-        <div class="image-team">
-          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate4">-->
-        </div>
-        <div class="identity">
-          <h2>anthony</h2>
-          <h2>vila</h2>
-          <h4>Designer</h4>
-        </div>
-      </section>
-      <section class="identity-container">
-        <div class="image-team">
-          <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate5">-->
-        </div>
-        <div class="identity">
-          <h2>aurelien</h2>
-          <h2>sarrat</h2>
-          <h4>Designer</h4>
-        </div>
-      </section>
-    </main>
-    
+<main class="team">
+  <div class="containerProject">
+    <article class="containerArticleProject">
+      <div class="containerTextProject">
+        <h3>Qui somme nous ?</h3>
+        <p class="textArticle">
+          Cure est une application web 100% indépendante et gratuite.
+        </p>
+        <p class="textArticle">
+          Elle n’est par conséquent pas influencé et influençable ni par les laboratoires pharmaceutiques, ni
+          par les médecins.
+        </p>
+        <p class="textArticle">
+          Elle a pour but de renseigner la population sur les médicaments qu'elle utilise.
+        </p>
+        <p class="textArticle">
+          Soyons acteur de nos soins.
+        </p>
+      </div>
+    </article>
+  </div>
+  <h1 class="team-title">L'Équipe</h1>
+  <section class="identity-container">
+    <div class="image-team">
+      <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate1">-->
+    </div>
+    <div class="identity">
+      <h2>Antoine</h2>
+      <h2>Baudot</h2>
+      <h4>Designer</h4>
+    </div>
+  </section>
+  <section class="identity-container">
+    <div class="image-team">
+      <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate2">-->
+    </div>
+    <div class="identity">
+      <h2>louis</h2>
+      <h2>gazel</h2>
+      <h4>Designer</h4>
+    </div>
+  </section>
+  <section class="identity-container">
+    <div class="image-team">
+      <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate3">-->
+    </div>
+    <div class="identity">
+      <h2>nathan</h2>
+      <h2>grigné</h2>
+      <h4>Designer</h4>
+    </div>
+  </section>
+  <section class="identity-container">
+    <div class="image-team">
+      <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate4">-->
+    </div>
+    <div class="identity">
+      <h2>anthony</h2>
+      <h2>vila</h2>
+      <h4>Designer</h4>
+    </div>
+  </section>
+  <section class="identity-container">
+    <div class="image-team">
+      <!--<img src="<? URL ?>assets/images/arnaud_skieur.png" alt="Teammate5">-->
+    </div>
+    <div class="identity">
+      <h2>aurelien</h2>
+      <h2>sarrat</h2>
+      <h4>Designer</h4>
+    </div>
+  </section>
+</main>
+
 <?php include './views/partials/footer.php' ?>
