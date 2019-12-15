b00c91de896f2af6619f2884db9240b5a7ece7fb
nathangrigne <nathan.grigne@hetic.net>
2019-12-15 16:25:48 +0100

    add view medocsList

---
+++ views/pages/medocsList.php
@@ -0,0 +1,17 @@
+<?php include './views/partials/header.php' ?>
+
+<? if(!empty($info_cip_bdpm) || !empty($info_cis_bdpm)): ?>
+
+<? foreach($info_cis_bdpm as $index=>$medicament): ?>
+    <div>
+        <a href="<?= URL.'medicament/'.$code_cip[$index]->code_cip13 ?>"><?= $medicament->denomination_medicament ?></a>
+    </div>
+<? endforeach; ?>
+
+<? else: ?>
+    <main>
+        <h1>SORRY RIEN TROUVER GROS</h1>
+    </main>
+<? endif; ?>
+
+<?php include './views/partials/footer.php' ?>
