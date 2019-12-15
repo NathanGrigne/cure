dfc42a891d82bf42207f01c5f11811210e3ca62b
nathangrigne <nathan.grigne@hetic.net>
2019-12-15 16:47:23 +0100

    update view medocsList

--- views/pages/medocsList.php
+++ views/pages/medocsList.php
@@ -1,16 +1,19 @@
 <?php include './views/partials/header.php' ?>
 
 <? if(!empty($info_cip_bdpm) || !empty($info_cis_bdpm)): ?>
+<main>
 
-<? foreach($info_cis_bdpm as $index=>$medicament): ?>
-    <div class="containerCardMedic">
-        <div class="cardMedic">
-            <div class="medic">
-                <a href="<?= URL.'medicament/'.$code_cip[$index]->code_cip13 ?>"><?= $medicament->denomination_medicament ?></a>
+    <? foreach($info_cis_bdpm as $index=>$medicament): ?>
+        <div class="containerCardMedic">
+            <div class="cardMedic">
+                <div class="medic">
+                    <a href="<?= URL.'medicament/'.$code_cip[$index]->code_cip13 ?>"><?= $medicament->denomination_medicament ?></a>
+                </div>
             </div>
         </div>
-    </div>
-<? endforeach; ?>
+    <? endforeach; ?>
+
+</main>
 
 <? else: ?>
     <main>
