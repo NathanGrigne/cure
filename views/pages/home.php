ecd54d7df389a8709001a8d7dca943dfc6d7dd36
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2019-12-16 13:34:09 +0100

    add content and modif css home page

--- views/pages/home.php
+++ views/pages/home.php
@@ -13,7 +13,7 @@
                 </p>
             </div>
         </article>
-        <div class="containerIllustrationSearch">    
+        <div class="containerIllustrationSearch">
             <div class="illustrationSearch">
             </div>
             <div class="containerScrollProject">
@@ -22,24 +22,82 @@
         <article class="containerArticleProject white">
             <div class="containerTextProject">
                 <p class="textArticle">
-                    Pour vous <span class="bold">aider, guider, renseigner</span> nous avons créé <span class="bold">Cure !</span>
+                    Pour vous <span class="bold">aider, guider, renseigner</span> nous avons créé <span
+                        class="bold">Cure !</span>
                 </p>
                 <p class="textArticle">
                     Notre but, rendre <span class="bold">l’automédication</span> plus
-                    <span class="bold">accessible</span>  afin que vous preniez <span class="bold">moins de risque.</span>
+                    <span class="bold">accessible</span> afin que vous preniez <span class="bold">moins de
+                        risque.</span>
                 </p>
             </div>
         </article>
-        <article class="containerArticleProject">
+        <article class="containerArticleProject ">
             <div class="containerTextProject">
+
                 <p class="textArticle">
-                En France <span class="bold">l’automédication</span>
-                </p> 
+                    En France <span class="bold">l’automédication</span>
+                </p>
                 <p class="textArticle">est <span class="bold">déconseillée</span>.
 
                 </p>
             </div>
         </article>
+        <div class="containerTitleProject">
+            <h1>Notre Projet </h1>
+            <div class="scrollDown"></div>
+        </div>
+        <article class="containerArticleProject white">
+            <div class="containerTextProject">
+                <p class="textArticle">
+                    Permettre à chacun d’avoir accès aux <span class="bold">informations primordiales</span> sur les
+                    médicaments.
+                </p>
+                <p class="textArticle">
+                    <span class="bold">Simplifier</span> la compréhension de vos médicaments.
+                </p>
+                <p class="textArticle">
+                    Vous aidez et vous conseillez afin de<span class="bold">faire les bons choix</span> lors de votre
+                    automédication.
+                </p>
+                <p class="textArticle">
+                    De plus, Cure est<span class="bold">100 % indépendant</span> donc totalement transparent avec vous.
+                </p>
+                <p class="textArticle">
+                    Et<span class="bold">aucune</span> de vos données n'est collectée.
+                </p>
+            </div>
+        </article>
+        <article class="containerArticleProject">
+            <div class="containerTextProject">
+                <h2>Comment nous pouvons vous aider ?
+                </h2>
+                <p class="textArticle">
+                    Pour vous aider nous avons étudiés pour <span class="bold">chaque symptôme</span> quel est le<span
+                        class="bold">médicament le plus adapté</span>, ainsi, nous pouvons vous conseiller.
+                </p>
+                <p class="textArticle">
+                    De plus, vous trouverez pour chaque médicament des éléments utiles :
+                </p>
+                <ul>
+                    <li><span class="bold">notice simplifiée </span></li>
+                    <li><span class="bold">effets secondaires </span></li>
+                    <li><span class="bold">composition et mode d’emploi </span></li>
+                </ul>
+                <p class="textArticle">
+                    Ainsi que beaucoup d’autres informations.
+                </p>
+                <p class="textArticle">
+                    Pour en découvrir plus sur vos médicaments ou pour tester Cure, cliquez sur le
+                    <span class="bold">bouton scannez ci-dessous.</span>
+                </p>
+
+            </div>
+        </article>
+        <div class="callToActionScan">
+            <h3> Votre recherche ! </h3>
+            <div class="imgScan"></div>
+        </div>
     </div>
 </main>
 
