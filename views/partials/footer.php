0a0f465e3cec4ec1bfdd5db4ddc446b50900a1b2
nathangrigne <nathan.grigne@hetic.net>
2019-12-17 20:58:53 +0100

    update view footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -2,12 +2,12 @@
         
     </footer>
     <? if($q != 'welcome'): ?>
-    <script src="<?= URL ?>assets/scripts/script.js"></script>
+        <script src="<?= URL ?>assets/scripts/script.js"></script>
     <? endif; ?>
-    <script src="<?= URL ?>assets/scripts/<?= $controller ?>.js"></script>
+        <script src="<?= URL ?>assets/scripts/<?= $controller ?>.js"></script>
     <? if($q == 'search'): ?>
-    <script src="https://cdn.jsdelivr.net/npm/scandit-sdk@4.x"></script>
-    <script src="<?= URL ?>assets/scripts/search2.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/scandit-sdk@4.x"></script>
+        <script src="<?= URL ?>assets/scripts/search2.js"></script>
     <? endif; ?>
 </body>
 </html>
