87d91ada73809366aa348cdc41c12f5439a14537
nathangrigne <nathan.grigne@hetic.net>
2019-12-17 20:57:55 +0100

    update script search scan

--- assets/scripts/search2.js
+++ assets/scripts/search2.js
@@ -1,10 +1,9 @@
 /* SCAN */
-ScanditSDK.configure("AUld4glrFxQDBTOWTg8Tv0ApoDD8QfwzNlhDAX5E/cIeTR629VoIE75yt4ugevHc0EmpUtZGfV/0VeU+d1xS5o59PRS9RNCL5Fk/QuEtm2bnJGjorCExPhclZ/oUEUR7x0R5b+ITkb+z2DMmiD+eF3BIIQNm3Hs5ralyr5TwGHLTmF+jO9YXOcqwN3xltz21+O1U2ER9tLq49GEwzx1ptFy0Ccknxz6SL39cM7lfKUMog6T8Tl4840XQvfGJ64aMdYsrgFiUe6OUOraCZ5Dz0MqZSCWSWpijtIg6vqGEcGxWOUGE0eUkR4vIBjT8IGibdEty8prHrWNUmm1WrljISvUslc543ZwGsoN+4BOlgFEw55yUb9zCz2Gu9MjY8sQ1TdNOGeOJyWU6iBzx5/RL550sFBsD08acWqR4U1WerzcgqVXTz5itUKSYcVVWvxzDBN1gAxgGWXJH4utBl5mHh8eASn7tY3t+0nJneBArELlbSA1OzNloE9HiBtlzWu6H8U2AnYVXB92Ub/MZl18WeiInlZK6xBnReSbA9lMcIcVtv4k4kPvGbim8G2GGyr+kQix3dlQzjE+G4lEFvyXc5dMUyQz5rIUbtmy6KJ6pt6ATy98bbIFAk3tYlTgiIuVDh2pMHmlFfMAZ6pR42fT6CA4wWpmn+wtbJtad6Sy4Z9smZNAMy1h6KSdKUGBT0pZeOPd5n6MOw8f1tOjzrJiKSReObCFjIWztWnBt+aMkE0bha93MA2iU6unrgPES309GvXIBBTS/TaRNGxc9qYFedYFzHpxWN4jwMOKGYD80sgQp/w==",{
-    //Remplace par ta clé web au dessus
+ScanditSDK.configure("ATBtxQ5uECNnDmdj1wEqdE0bfXc2EEaV52TknMZaeoIGAWymFwq6C85LJHJcTbfiQFrPMANudVqaXfo4Al0Gsd17UpHjXG7SuXti94lCw9JyBp0IRwaKIpAcsKN8CXxQ9Czzp3jFlXri9K0xn8vnV9Va1TTgSC5R83Iw99e2APo/mardlDrFcWnVYukAvHCVx1vG3N3skoWO6yVeZlietLzHj76aBUeSLxmK9aKV35xU27v63BVvZ4kzTY0HFAQvDRyJHK91eQvFoaL9NMWYl5nRaGrDL/jwBwp9T86S3tajCyWE6u2R4gFh+5WuSORTgEYUw6QK5fBKm9ZaTGIPsWQgRv9U66A0aTUUiPe3C7PJkKOT1Lu+a0s6+TKRZllPqd9izYOkqwf4pIAwLhnCF457q+VQqHqoCldV8kubl3R68DvImUxs2BIwbuqW1pBZn36KqIRtcbF6qst0wnQykbIlZn755qVlYwjbZNEg8Pswn3KlZZFxvS/kPjZ1ypiJ9Kn+cHj7hkxs66LuqGfktygOIAuN5bbPm4F6rwPkAxzXl5EYAllPtyPgPc+L8RZW6x42J+I6p4PWvNPncnGAKsKys4OjzSHFabtpWNYr7xllfGttnmS4x1kDaYg9okzmPJwnD+/e3TGIOtPlVEnrUtxOKmRyZ+pnReiaJcSUPbjJnhpeNOjfMqgx8wBnoypemI/CZF3aBhi97tC1QlzZcQHx48ItC7GXTdijHpYhLCPy7Bf6hguVtgpz65pU1Z+a+qe/hEvofLAfCTjMe1xD8TIHi9CXNE8SMrnR6MCugvh/vw==",{
     engineLocation: "https://cdn.jsdelivr.net/npm/scandit-sdk/build"
 });
 ScanditSDK.BarcodePicker.create(document.getElementById("scandit-barcode-picker"), {
-    playSoundOnScan: true,
+    playSoundOnScan: false,
     vibrateOnScan: true
 }).then(function(barcodePicker) {
     var scanSettings = new ScanditSDK.ScanSettings({
@@ -13,8 +12,10 @@ ScanditSDK.BarcodePicker.create(document.getElementById("scandit-barcode-picker"
     });
     barcodePicker.applyScanSettings(scanSettings);
     barcodePicker.on("scan", function(scanResult) {
-        alert(scanResult.barcodes.reduce(function(string, barcode) {
-            return string + ScanditSDK.Barcode.Symbology.toHumanizedName(barcode.symbology) + ": " + barcode.data + "\n";
+        let code = (scanResult.barcodes.reduce(function(string, barcode) {
+            return string + barcode.data;
         }, ""));
+        code = code.substr(3, 13);
+        document.location = `medicament/${code}`;
     });
 });
