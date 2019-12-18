5ff6f1aaece3056d14908576114fb96d891e4a2c
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2019-12-18 20:58:27 +0100

    update content and style home page

--- views/pages/home.php
+++ views/pages/home.php
@@ -3,16 +3,6 @@
 <main>
 
     <div class="containerProject">
-        <article class="containerArticleProject">
-            <div class="containerTextProject">
-                <p class="textArticle">
-                    Aujourd’hui, <span class="bold">80 % </span>des Français ont déjà eu recours à
-                    <span class="bold">l’automédication</span>
-                    et le plus souvent pour des pathologies classiques (rhume, maux de tête et de
-                    gorge).
-                </p>
-            </div>
-        </article>
         <div class="containerIllustrationSearch">
             <div class="illustrationSearch">
             </div>
@@ -30,51 +20,12 @@
                     <span class="bold">accessible</span> afin que vous preniez <span class="bold">moins de
                         risque.</span>
                 </p>
-            </div>
-        </article>
-        <article class="containerArticleProject ">
-            <div class="containerTextProject">
-
-                <p class="textArticle">
-                    En France <span class="bold">l’automédication</span>
-                </p>
-                <p class="textArticle">est <span class="bold">déconseillée</span>.
-
-                </p>
-            </div>
-        </article>
-        <div class="containerTitleProject">
-            <h1>Notre Projet </h1>
-            <div class="scrollDown"></div>
-        </div>
-        <article class="containerArticleProject white">
-            <div class="containerTextProject">
-                <p class="textArticle">
-                    Permettre à chacun d’avoir accès aux <span class="bold">informations primordiales</span> sur les
-                    médicaments.
+                <p class="textArticle boldTextArticle">
+                    Prenez moins de risque lors de votre automédication
                 </p>
                 <p class="textArticle">
-                    <span class="bold">Simplifier</span> la compréhension de vos médicaments.
-                </p>
-                <p class="textArticle">
-                    Vous aidez et vous conseillez afin de<span class="bold">faire les bons choix</span> lors de votre
-                    automédication.
-                </p>
-                <p class="textArticle">
-                    De plus, Cure est<span class="bold">100 % indépendant</span> donc totalement transparent avec vous.
-                </p>
-                <p class="textArticle">
-                    Et<span class="bold">aucune</span> de vos données n'est collectée.
-                </p>
-            </div>
-        </article>
-        <article class="containerArticleProject">
-            <div class="containerTextProject">
-                <h2>Comment nous pouvons vous aider ?
-                </h2>
-                <p class="textArticle">
-                    Pour vous aider nous avons étudiés pour <span class="bold">chaque symptôme</span> quel est le<span
-                        class="bold">médicament le plus adapté</span>, ainsi, nous pouvons vous conseiller.
+                    Pour vous aider nous avons étudié pour <span class="bold">chaque symptôme</span> quel est
+                    le <span class="bold">médicament le plus adapté</span>, ainsi, nous pouvons vous conseiller.
                 </p>
                 <p class="textArticle">
                     De plus, vous trouverez pour chaque médicament des éléments utiles :
@@ -84,13 +35,18 @@
                     <li><span class="bold">effets secondaires </span></li>
                     <li><span class="bold">composition et mode d’emploi </span></li>
                 </ul>
+
                 <p class="textArticle">
                     Ainsi que beaucoup d’autres informations.
+
                 </p>
                 <p class="textArticle">
-                    Pour en découvrir plus sur vos médicaments ou pour tester Cure, cliquez sur le
-                    <span class="bold">bouton scannez ci-dessous.</span>
-                </p>
+                    Pour en découvrir plus sur
+                        vos médicaments ou pour tester Cure, cliquez sur le<span class="bold">bouton scannez ci-dessous</span>.
+                <p>
+                <p class="boldTextArticle">
+                        Avec Cure vous avez les clés en main pour vous soigner !
+                    </p>
 
             </div>
         </article>
