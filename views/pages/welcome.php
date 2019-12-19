ffde97c2f60edc751f8cddef36dd5fb76f4571be
nathangrigne <grigne.nathan@gmail.com>
2019-12-19 15:04:00 +0100

    update view welcome

--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -1,12 +1,12 @@
 <?php include './views/partials/header.php' ?>
         <main class="main">
             <div class="first-page">
-                <div class="logo-container">
-                    <!--<img src="<?= URL ?>assets/images/logo_welcome.png" alt="" class="logo-welcome">-->
-                </div>
+                <!--Logo on the welcome Page-->
+                <div class="logo-container"></div>
                 <div class="text-container">
                     <h1 class="title-welcome">Bienvenue !</h1>
                     <p class="text-welcome">Viens découvrir si tu utilise correctement tes médicaments </p>
+                    <!--Link to the Home -->
                     <button class="let-go"><a class="link-let-go" href="<?= URL ?>home">C'est parti !</a></button>
                 </div>
             </div>
