17044cb3c5462a027bbd65db607ec88cbcae3bb8
nathangrigne <grigne.nathan@gmail.com>
2019-12-19 15:25:41 +0100

    update script search

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,10 +1,10 @@
 const scanTop = document.querySelector('.infos-scan-top')
 const scanBottom = document.querySelector('.infos-scan')
 
+//Display help to find CIP code and DataMatrix
 scanTop.addEventListener('click', () =>{
     infosCip.classList.remove('hide')
 })
-
 scanBottom.addEventListener('click', () =>{
     infosMatrix.classList.remove('hide')
 })
@@ -14,6 +14,7 @@ const close2 = document.querySelector('.close2')
 const infosCip = document.querySelector('.infos-cip')
 const infosMatrix = document.querySelector('.infos-matrix')
 
+//Hide help to find CIP code and DataMatrix
 close.addEventListener('click', () =>{
     infosCip.classList.add('hide')
 })
