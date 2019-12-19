f31edede8bf5cf71c61136c87736e6adf5fe5ee0
nathangrigne <grigne.nathan@gmail.com>
2019-12-19 15:24:46 +0100

    update script medocs

--- assets/scripts/medocs.js
+++ assets/scripts/medocs.js
@@ -1,6 +1,7 @@
 const titleMedic = document.querySelectorAll('.infoMedic h2')
 const containerTextInfoMedic = document.querySelectorAll('.containerTextInfoMedic')
 
+//Open or close the div, close the other when an other is open
 for(let i = 0; i<titleMedic.length;i++) {
   titleMedic[i].addEventListener(
       'click',
