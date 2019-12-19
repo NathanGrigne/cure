d0f3038d1f9f14fa0ef640f165108437f965471e
nathangrigne <grigne.nathan@gmail.com>
2019-12-19 15:04:19 +0100

    update view footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,10 +1,13 @@
     <footer>
         
     </footer>
+    <!--Chech if the URL is different as 'Welcome' and display the script if it is-->
     <? if($q != 'welcome'): ?>
         <script src="<?= URL ?>assets/scripts/script.js"></script>
     <? endif; ?>
-        <script src="<?= URL ?>assets/scripts/<?= $controller ?>.js"></script>
+    <!--Always tru to load the script if he contains the name of the controllers-->
+    <script src="<?= URL ?>assets/scripts/<?= $controller ?>.js"></script>
+    <!--Check if the page is 'Search', if its is and load this scripts-->
     <? if($q == 'search'): ?>
         <script src="https://cdn.jsdelivr.net/npm/scandit-sdk@4.x"></script>
         <script src="<?= URL ?>assets/scripts/search2.js"></script>
