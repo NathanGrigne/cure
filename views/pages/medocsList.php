c593bd878743db8c23887b56bcf44ce4a8be44ed
nathangrigne <grigne.nathan@gmail.com>
2019-12-19 15:02:50 +0100

    update view medocsList

--- views/pages/medocsList.php
+++ views/pages/medocsList.php
@@ -1,8 +1,10 @@
+<!--Integration header-->
 <?php include './views/partials/header.php' ?>
 
+<!--Check if variables are empty or not, if it isn't deiplay the list-->
 <? if(!empty($info_cip_bdpm) || !empty($info_cis_bdpm)): ?>
 <main>
-
+    <!--Creation of a div for each medicine-->
     <? foreach($info_cis_bdpm as $index=>$medicament): ?>
         <div class="containerCardMedic">
             <div class="cardMedic">
@@ -12,13 +14,14 @@
             </div>
         </div>
     <? endforeach; ?>
-
 </main>
 
+<!--If variables are empty, display this text-->
 <? else: ?>
     <main>
-        <h1>SORRY RIEN TROUVER GROS</h1>
+        <h1>Désolé, aucun médicament ne correspond à votre recherche.</h1>
     </main>
 <? endif; ?>
 
+<!--Integration footer-->
 <?php include './views/partials/footer.php' ?>
