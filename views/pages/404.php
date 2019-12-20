254df7156f99994cb47044f53aefd11a55e909fd
nathangrigne <grigne.nathan@gmail.com>
2019-12-20 10:40:55 +0100

    update view 404 page

--- views/pages/404.php
+++ views/pages/404.php
@@ -1,6 +1,9 @@
 <?php include './views/partials/header.php' ?>
-<h1>Oups! erreur 404</h1>
 
-<a href="<? URL ?>home">Revenir à la home</a>
+<div class="container-404">
+    <h1>Oups! erreur 404</h1>
+
+    <a href="<? URL ?>home">Revenir à la home</a>
+</div>
 
 <?php include './views/partials/footer.php' ?>
